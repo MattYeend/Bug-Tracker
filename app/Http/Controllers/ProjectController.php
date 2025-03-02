e220a0da7f40557a4f6f4a3c7487b1979ebaed94
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-03-02 09:08:36 +0000

    #7 New Project Controller

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
+use App\Repositories\ProjectRepository;
+use Inertia\Inertia;
+use Inertia\Response;
+use Illuminate\Http\RedirectResponse;
+
+class ProjectController extends Controller
+{
+    protected $repo;
+
+    public function __construct(ProjectRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Response
+    {
+        $projects = $this->repo->all();
+        return Inertia::render('Projects/Index', ['projects' => $projects]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Projects/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProjectRequest $request): RedirectResponse
+    {
+        $this->repo->create($request->validated());
+        return redirect()->route('projects.index')->with('success', 'Project created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id): Response
+    {
+        $project = $this->repo->find($id);
+        return Inertia::render('Projects/Show', ['project' => $project]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id): Response
+    {
+        $project = $this->repo->find($id);
+        return Inertia::render('Projects/Edit', ['project' => $project]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProjectRequest $request, $id): RedirectResponse
+    {
+        $project = $this->repo->find($id);
+        $this->repo->update($project, $request->validated());
+
+        return redirect()->route('projects.index')->with('success', 'Project updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id): RedirectResponse
+    {
+        $project = $this->repo->find($id);
+        $this->repo->delete($project);
+
+        return redirect()->route('projects.index')->with('success', 'Project deleted.');
+    }
+}
